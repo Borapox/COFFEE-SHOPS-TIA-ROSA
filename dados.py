4b456bd80e0d3bfa37398e12014458e538acd815
Müller <muller.mdi@gmail.com>
2025-05-18 21:23:58 -0300

    criado arquivo dados.py para armazenamento de dados dos clientes (banco de dados). - clientes.py desestruturado para cadastro e vizualização de clientes

---
+++ dados.py
@@ -0,0 +1,63 @@
+# Dados de clientes da cafeteria savos 
+clientes = {
+    1: {
+        "nome": "João Silva",
+        "cpf": "123.456.789-00",
+        "telefone": "(61) 98765-4321",
+        "endereco": "Asa Sul, Quadra 308, Bloco B, Brasília - DF",
+    },
+    2: {
+        "nome": "Maria Oliveira",
+        "cpf": "987.654.321-00",
+        "telefone": "(61) 91234-5678",
+        "endereco": "Asa Norte, SQN 209, Bloco C, Brasília - DF",
+    },
+    3: {
+        "nome": "Carlos Pereira",
+        "cpf": "456.789.123-00",
+        "telefone": "(61) 93456-7890",
+        "endereco": "Taguatinga Norte, QNA 12, Brasília - DF",
+    },
+    4: {
+        "nome": "Ana Souza",
+        "cpf": "321.654.987-00",
+        "telefone": "(61) 99876-1234",
+        "endereco": "Sudoeste, CLSW 302, Brasília - DF",
+    },
+    5: {
+        "nome": "Pedro Lima",
+        "cpf": "789.123.456-00",
+        "telefone": "(61) 99654-3210",
+        "endereco": "Lago Sul, QI 15, Conjunto 4, Brasília - DF",
+    },
+    6: {
+        "nome": "Juliana Mendes",
+        "cpf": "654.987.321-00",
+        "telefone": "(61) 99123-4567",
+        "endereco": "Lago Norte, QL 10, Conjunto 3, Brasília - DF",
+    },
+    7: {
+        "nome": "Rafael Torres",
+        "cpf": "159.753.486-00",
+        "telefone": "(61) 98547-9630",
+        "endereco": "Águas Claras, Rua das Pitangueiras, Brasília - DF",
+    },
+    8: {
+        "nome": "Fernanda Castro",
+        "cpf": "951.357.284-00",
+        "telefone": "(61) 98213-8492",
+        "endereco": "Guará II, QE 40, Conjunto G, Brasília - DF",
+    },
+    9: {
+        "nome": "Lucas Rocha",
+        "cpf": "852.741.963-00",
+        "telefone": "(61) 98741-2586",
+        "endereco": "Samambaia Sul, QS 304, Conjunto 5, Brasília - DF",
+    },
+    10: {
+        "nome": "Beatriz Ferreira",
+        "cpf": "753.951.852-00",
+        "telefone": "(61) 98963-1472",
+        "endereco": "Plano Piloto, SHCN 210, Bloco A, Brasília - DF",
+    }
+}
